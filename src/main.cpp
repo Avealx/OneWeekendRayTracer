0be1c7f3df7fe8e63267fe218c5d0a80ca0a7e08
Felix Herter <herterfe@gmail.com>
2023-04-02 13:36:44 +0200

    add: progress report during image printing

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' <<  std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = static_cast<double>(i) / (image_width - 1);
             auto g = static_cast<double>(j) / (image_height - 1);
@@ -21,4 +22,5 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    std:: cerr << "\nDone.\n";
 }

935236a67993f786a80aa8ea2f384d315cf25c4d
Felix Herter <herterfe@gmail.com>
2023-04-23 19:13:22 +0200

    enh: introduce face sides (front, back)

--- src/hit.hpp
+++ src/hit.hpp
@@ -2,27 +2,46 @@
 
 #include <ray.hpp>
 
-#include <iostream>
 #include <cmath>
+#include <iostream>
 #include <limits>
+#include <string>
 
+
+enum class FaceSide {front, back};
+
+std::string toString(FaceSide const side) {
+    switch(side) {
+        case FaceSide::front: return "front";
+        case FaceSide::back: return "back";
+    }
+    return "INVALID SIDE";
+}
 
 struct hit_record {
     point3 p;
     vec3 normal;
     double t;
+    FaceSide side;
 
     explicit operator bool() const { return ! std::isnan(t); }
 
     static constexpr hit_record miss() {
-        return hit_record{{}, {}, std::numeric_limits<double>::quiet_NaN()};
+        return hit_record{{}, {}, std::numeric_limits<double>::quiet_NaN(), FaceSide::front};
+    }
+
+    void set_face_normal(ray const & r, vec3 const & outward_normal) {
+        side = dot(r.d, outward_normal) < 0 ? FaceSide::front : FaceSide::back;
+        normal = side == FaceSide::front ? outward_normal : -outward_normal;
     }
 };
 
 std::ostream& operator<<(std::ostream& out, hit_record const & hr) {
     return out << '(' <<   "p=" << hr.p
                       << ", n=" << hr.normal
-                      << ", t=" << hr.t << ')' ;
+                      << ", t=" << hr.t
+                      << ", " << toString(hr.side)
+                       << ')';
 }
 
 bool operator==(hit_record const & lhs, hit_record const & rhs) {
--- src/hit.test.cpp
+++ src/hit.test.cpp
@@ -17,8 +17,8 @@ TEST(hit_record, has_aggregate_constructor) { hit_record{point3{}, vec3{}, 0.0};
 
 TEST(hit_record, can_be_printed) {
     std::stringstream ss{};
-    ss << hit_record{point3{1.0, 2.0, 3.0}, vec3{1.0, 2.0, 3.0}, 5.0};
-    EXPECT_THAT(ss.str(), Eq("(p=(1, 2, 3), n=(1, 2, 3), t=5)"));
+    ss << hit_record{point3{1.0, 2.0, 3.0}, vec3{1.0, 2.0, 3.0}, 5.0, FaceSide::front};
+    EXPECT_THAT(ss.str(), Eq("(p=(1, 2, 3), n=(1, 2, 3), t=5, front)"));
 }
 
 TEST(hit_record, has_can_be_cast_to_bool_true) {
@@ -53,6 +53,20 @@ TEST(hit_record, equality_false_hit_hit) {
     EXPECT_THAT(hr, Ne(hr1));
     EXPECT_THAT(hr, Ne(hr2));
     EXPECT_THAT(hr, Ne(hr3));
+}
+
+TEST(hit_record, has_correct_side_when_ray_is_outside) {
+    hit_record hr = hit_record{point3{0.0}, vec3{0.0, 0.0, -1.0}, 0.0, FaceSide::front};
+    vec3 const outward_normal = vec3{0.0, 0.0, 1.0};
+    hr.set_face_normal(ray{point3{0.0, 0.0, 1.0}, outward_normal}, outward_normal);
+    EXPECT_THAT(hr.normal, Eq(-outward_normal));
+}
+
+TEST(hit_record, has_correct_side_when_ray_is_inside) {
+    hit_record hr = hit_record{point3{0.0}, vec3{0.0, 0.0, -1.0}, 0.0, FaceSide::front};
+    vec3 const outward_normal = vec3{0.0, 0.0, 1.0};
+    hr.set_face_normal(ray{point3{0.0, 0.0, 1.0}, -outward_normal}, outward_normal);
+    EXPECT_THAT(hr.normal, Eq(outward_normal));
 }
 
 int main(int argc, char **argv)

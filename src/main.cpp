4f0f6c891f50d10c07ad5ae27f396e3e8f9af414
Felix Herter <herterfe@gmail.com>
2023-05-03 21:06:49 +0200

    enh: main: draw diffuse sphere

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,15 @@
 #include <memory>
 
 
-color ray_color(ray const & r, hittable_I const & world) {
+color ray_color(ray const & r, hittable_I const & world, int depth) {
+    if (depth <= 0)
+        return color{0.0, 0.0, 0.0};
+
     hit_record rec = world.hit(r, 0, infinity);
-    if (rec)
-        return 0.5 * (rec.normal + color{1.0, 1.0, 1.0});
+    if (rec) {
+        point3 const target = rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray{rec.p, target - rec.p}, world, depth - 1); // half absorbed?
+    }
 
     vec3 const unit_direction = unit_vector(r.d);
     auto t = 0.5 * (unit_direction.y + 1.0);
@@ -27,6 +32,7 @@ int main() {
     int const image_width = 600;
     int const image_height = static_cast<int>(image_width / aspect_ratio);
     int const samples_per_pixel = 100;
+    int const max_depth = 50;
 
     // World
     hittable_list world;
@@ -35,14 +41,6 @@ int main() {
 
     // Camera
     camera const cam;
-    //auto viewport_height = 2.0;
-    //auto viewport_width = viewport_height / aspect_ratio;
-    //auto focal_length = 1.0;
-
-    //auto origin = point3{0, 0, 0};
-    //auto horizontal = vec3{viewport_width, 0, 0};
-    //auto vertical = vec3{0, viewport_height, 0};
-    //auto lower_left_corner = origin - 0.5*horizontal - 0.5*vertical - vec3{0, 0, focal_length};
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -55,12 +53,8 @@ int main() {
                 auto w = (i + random_double()) / (image_width - 1);
                 auto h = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(w, h);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
-            //auto h = static_cast<double>(i) / (image_width  - 1);
-            //auto v = static_cast<double>(j) / (image_height - 1);
-            //ray const r{origin, lower_left_corner + h*horizontal + v*vertical - origin};
-            //color const pixel_color = ray_color(r, world);
 
             write_color(std::cout, pixel_color, samples_per_pixel, ns_color::WritePretty{false});
             std::cout << " ";

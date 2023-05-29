005413509e15fc97f7c654b99aca4e99bdcb1372
Felix Herter <herterfe@gmail.com>
2023-05-29 10:14:29 +0200

    add: hollow sphere + rearrange

--- src/main.cpp
+++ src/main.cpp
@@ -36,20 +36,21 @@ int main() {
     auto const aspect_ratio = 16.0 / 9.0;
     int const image_width = 600;
     int const image_height = static_cast<int>(image_width / aspect_ratio);
-    int const samples_per_pixel = 100;
+    int const samples_per_pixel = 1000;
     int const max_depth = 50;
 
     // World
     hittable_list world;
 
     auto material_ground = std::make_shared<lambertian>(color{0.4, 0.5, 0.6});
-    auto material_center = std::make_shared<dielectric>(1.1);
-    auto material_left =   std::make_shared<metal>(color{0.8, 0.8, 0.75}, 0.01);
-    auto material_right =   std::make_shared<metal>(color{0.8, 0.6, 0.2}, 0.3);
+    auto material_left = std::make_shared<dielectric>(1.3);
+    auto material_center = std::make_shared<metal>(color{0.8, 0.8, 0.75}, 0.01);
+    auto material_right = std::make_shared<metal>(color{0.8, 0.6, 0.2}, 0.3);
 
     world.add(std::make_shared<Sphere>(point3{0.0, -100.5, -1.0}, 100.0, material_ground));
     world.add(std::make_shared<Sphere>(point3{ 0.0, 0.0, -1.0}, 0.5, material_center));
     world.add(std::make_shared<Sphere>(point3{-1.0, 0.0, -1.0}, 0.5, material_left));
+    world.add(std::make_shared<Sphere>(point3{-1.0, 0.0, -1.0}, -0.35, material_left));
     world.add(std::make_shared<Sphere>(point3{ 1.0, 0.0, -1.0}, 0.5, material_right));
 
     // Camera

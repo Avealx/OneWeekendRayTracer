a486c2e770d7d63c680f0c0dbf343a93f745a3ba
Felix Herter <herterfe@gmail.com>
2023-08-08 10:18:38 +0200

    add: texture coordinates can be compared for equality
    Injecting the functionality is nice, but I'm should check the efficiency of
    the iterator based operations (iterating through all elements and compare for
    equality vs compare all components in a single statement).
    Also inheriting from all the crtps means that aggregate initialization gets
    clumsy (aka agg = {{},{},{}, 1.0, 2.0}).

--- src/texture.hpp
+++ src/texture.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 #include <color.hpp>
+#include <crtp_functionality.hpp>
 #include <vec3.hpp>
 
 
-struct TextureCoordinates2d {
+struct TextureCoordinates2d : inject_pointer_as_iterator<double>
+                            , inject_trivial_begin_end<TextureCoordinates2d>
+                            , inject_container_equal<TextureCoordinates2d> {
     double u{-1.0}, v{-1.0};
+
+    TextureCoordinates2d() = default;
+    TextureCoordinates2d(double const u, double const v) : u{u}, v{v} {}
 };
 
 struct TextureI {

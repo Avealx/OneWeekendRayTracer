4d9784db3eca15b397ca407af2c579d88b1aaa41
Felix Herter <herterfe@gmail.com>
2023-08-14 23:11:28 +0200

    add: hittables, RotatedY

--- src/hittables.hpp
+++ src/hittables.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <hit.hpp>
+
+#include <algorithm>
 #include <memory>
+#include <ranges>
 
 class Translated : public HittableI {
 public:
@@ -26,5 +29,55 @@ HitRecord Translated::hit(Ray const & ray, double t_min, double t_max) const {
     Ray const translated_ray{ray.o - translation_, ray.d, ray.t};
     auto hit_record = hittable_->hit(translated_ray, t_min, t_max);
     hit_record.p += translation_;
+    return hit_record;
+}
+
+class RotatedY : public HittableI {
+public:
+    RotatedY(std::shared_ptr<HittableI> hittable, double const angle_degree)
+        : hittable_{hittable}
+        , sin_theta_{std::sin(degree_to_radians(angle_degree))}
+        , cos_theta_{std::cos(degree_to_radians(angle_degree))}
+        , aabb_{compute_bounding_box()} {}
+
+    // HittableI
+    HitRecord hit(Ray const & r, double t_min, double t_max) const override;
+    Aabb bounding_box(TimeInterval times) const override{ return aabb_; };
+private:
+
+    vec3 rotate_y(vec3 const & vec) const {
+        return vec3{cos_theta_ * vec.x + sin_theta_ * vec.z,
+                    vec.y,
+                    -sin_theta_ * vec.x + cos_theta_ * vec.z};
+    }
+
+    vec3 rotate_inverse_y(vec3 const & vec) const {
+        return vec3{cos_theta_ * vec.x - sin_theta_ * vec.z,
+                    vec.y,
+                    sin_theta_ * vec.x + cos_theta_ * vec.z};
+    }
+
+    Aabb compute_bounding_box() const {
+        auto const original_corners = get_all_corners(hittable_->bounding_box(TimeInterval{}));
+        vec3 min_corner{infinity}, max_corner{-infinity};
+        for (auto const & rotated_corner : original_corners | std::views::transform([this](vec3 const & v){ return rotate_y(v); })) {
+            min_corner = min_components(min_corner, rotated_corner);
+            max_corner = max_components(max_corner, rotated_corner);
+        }
+        return Aabb{AabbBounds{min_corner, max_corner}};
+    }
+
+
+    std::shared_ptr<HittableI> hittable_;
+    double sin_theta_;
+    double cos_theta_;
+    Aabb aabb_;
+};
+
+HitRecord RotatedY::hit(Ray const & ray, double t_min, double t_max) const {
+    Ray const rotated_ray(rotate_inverse_y(ray.o), rotate_inverse_y(ray.d), ray.t);
+    auto hit_record = hittable_->hit(rotated_ray, t_min, t_max);
+    hit_record.p = rotate_y(hit_record.p);
+    hit_record.normal = rotate_y(hit_record.normal);
     return hit_record;
 }
--- src/hittables.test.cpp
+++ src/hittables.test.cpp
@@ -7,6 +7,7 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+using testing::DoubleNear;
 using testing::Eq;
 using testing::Test;
 
@@ -50,6 +51,45 @@ TEST_F(ATranslated, can_be_missed) {
     vec3 const direction{0.0 , 0.0, -1.0};
     Ray const ray{origin, direction};
     EXPECT_FALSE(translated.hit(ray, 0.0, infinity));
+}
+
+
+struct ARotatedY : Test {
+    vec3 const corner{-2.0, -0.5, 0.0};
+    vec3 const u{4.0, 0.0, 0.0};
+    vec3 const v{0.0, 1.0, 0.0};
+    std::shared_ptr<HittableI> original_quad_ptr = std::make_shared<Quad>(corner, u, v, std::make_shared<MockMaterial>());
+    double const rotation_degree{90.0};
+    RotatedY const rotated{original_quad_ptr, rotation_degree};
+
+    point3 const hit_point{0.0, 0.25, 1.0};
+    vec3 const hitting_ray_direction{-1.0, 0.0, 0.0};
+    Ray const hitting_ray{hit_point - hitting_ray_direction, hitting_ray_direction};
+
+    vec3 const missing_ray_direction{0.0, 0.0, 1.0};
+    Ray const missing_ray{corner + 0.25 * (u + v) - missing_ray_direction, missing_ray_direction};
+};
+
+void expect_double_equal(vec3 const & u, vec3 const & v) {
+    EXPECT_THAT(u.x, DoubleNear(v.x, 1e-8));
+    EXPECT_THAT(u.y, DoubleNear(v.y, 1e-8));
+    EXPECT_THAT(u.z, DoubleNear(v.z, 1e-8));
+}
+
+TEST_F(ARotatedY, has_correct_bounding_box) {
+    auto const aabb = rotated.bounding_box(TimeInterval{});
+    expect_double_equal(aabb.min(), point3{-5e-5 /*Aabb padding for quads*/, -0.5, -2.0});
+    expect_double_equal(aabb.max(), point3{ 5e-5 /*Aabb padding for quads*/,  0.5,  2.0});
+}
+
+TEST_F(ARotatedY, can_be_hit) {
+    auto const hit_record = rotated.hit(hitting_ray, 0.0, infinity);
+    EXPECT_TRUE(hit_record);
+    expect_double_equal(hit_record.p, hit_point);
+}
+
+TEST_F(ARotatedY, can_be_missed) {
+    EXPECT_FALSE(rotated.hit(missing_ray, 0.0, infinity));
 }
 
 
